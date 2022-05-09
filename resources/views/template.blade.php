ba2862ad2bf70d5485453e20958ecafa2b87941a
masato <tkmasa1024@gmail.com>
2022-05-10 00:48:35 +0900

    na

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,6 +7,18 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="{{asset('css/tailwind_sub.css')}}" rel="stylesheet">
   <title>学生ジェネレータ</title>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1151265093391964"
+     crossorigin="anonymous"></script>
+  <!-- stugenGeneral -->
+  <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1151265093391964"
+     data-ad-slot="1758144469"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+  <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
 </head>
 <body class="p-4">
 <header>
